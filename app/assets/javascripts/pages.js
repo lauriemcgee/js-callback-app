97de7d1cc853ba75cb29194c056fc6e4c0f92be1
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-20 20:37:58 -0500

    changed file--calling chicago data api with javascript and displaying json object in view

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,16 +1,8 @@
-function allCaps() {
-  setTimeout(function() {
-    document.querySelector("#first").style["color"] = "pink";
-    setTimeout(function() {
-      document.querySelector("#second").style["color"] = "pink";
-      setTimeout(function() {
-        document.querySelector("#third").style["color"] = "pink";
-        setTimeout(function() {
-          document.querySelector("#fourth").style["color"] = "pink";
-        }, 2000);
-      }, 2000);
-    }, 2000);
-  }, 2000);
-}
+/* $ global */
+
+$.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(data) {
+  console.log('data');
+  document.querySelector('#data').innerHTML = data;
+});
 
 
