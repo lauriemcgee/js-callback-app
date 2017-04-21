dc0c380fba620fc511b0088c006ddaf4171657bb
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-04-20 21:12:56 -0500

    displaying name and department

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,8 +1,12 @@
-/* $ global */
+/* global $ */
 
 $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(data) {
-  console.log('data');
-  document.querySelector('#data').innerHTML = data;
+  var htmlString = "";
+  for ( var i = 0; i < data.length; i++) {
+    htmlString += '<h4>' + data[i].name + '</h4>';
+    htmlString += '<p>' + data[i].department + '</p>';
+  }
+  document.querySelector('#employee-data').innerHTML = htmlString;
 });
 
 
